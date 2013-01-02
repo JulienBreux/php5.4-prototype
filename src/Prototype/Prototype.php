b5bbeeffc2f24619d7eb7141d3b8a5c9e0c6ca65
Julien Breux <julien.breux@gmail.com>
2013-01-02 20:41:48 +0100

    Fix code style

--- src/Prototype/Prototype.php
+++ src/Prototype/Prototype.php
@@ -11,29 +11,29 @@ trait Prototype
 	protected static $methods = [];
 
 	/**
-	 * Get property
-	 *
-	 * @param string $name Property name
-	 * @return mixed
-	 */
+	* Get property
+	*
+	* @param string $name Property name
+	* @return mixed
+	*/
 	public function &__get($name)
-    {
-        if(array_key_exists($name, $this->properties))
-        {
-            return $this->properties[$name];
-        }
+	{
+		if(array_key_exists($name, $this->properties))
+		{
+			return $this->properties[$name];
+		}
 
-        return null;
-    }
+		return null;
+	}
 
 	/**
-	 * Set property
-	 *
-	 * @param string $name Property or method name
-	 * @param mixed $value Property or method value
-	 */
+	* Set property
+	*
+	* @param string $name Property or method name
+	* @param mixed $value Property or method value
+	*/
 	public function __set($name, $value)
-    {
+	{
 		// Set as method
 		if( $value instanceof \Closure )
 		{
@@ -45,21 +45,21 @@ trait Prototype
 			self::$methods[__CLASS__][$name] = $value;
 		}
 		// Set as property
-        else
-        {
-            $this->properties[$name] = $value;
-        }
-    }
+		else
+		{
+			$this->properties[$name] = $value;
+		}
+	}
 
 	/**
-	 * Call method
-	 *
-	 * @param string $name Method name
-	 * @param array $arguments Method arguments
-	 * @return mixed
-	 */
+	* Call method
+	*
+	* @param string $name Method name
+	* @param array $arguments Method arguments
+	* @return mixed
+	*/
 	public function __call($name, $arguments = [])
-    {
+	{
 		if(array_key_exists(__CLASS__, self::$methods))
 		{
 			$methods = self::$methods[__CLASS__];
@@ -76,5 +76,5 @@ trait Prototype
 				return call_user_func_array($method, $arguments);
 			}
 		}
-    }
+	}
 }
